f4959b291a30bd48facff6bd4496ed9ccb977a2d
olegshch <st069439@student.spbu.ru>
2019-11-22 17:28:22 +0300

    вроде работает

---
+++ Lazy/LazyFactory.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace Lazy
+{
+    /// <summary>
+    /// Making Lazy
+    /// </summary>
+    /// <typeparam name="T">value type</typeparam>
+    public class LazyFactory<T>
+    {
+        /// <summary>
+        /// Simple Lazy
+        /// </summary>
+        /// <param name="function">function</param>
+        /// <returns>result</returns>
+        public static ILazy<T> CreateLazy(Func<T> function) => new Lazy<T>(function);
+        
+        /// <summary>
+        /// Multithread Lazy
+        /// </summary>
+        /// <param name="function">function</param>
+        /// <returns>result</returns>
+        public static ILazy<T> CreateMultiLazy(Func<T> function) => new MultiLazy<T>(function);
+    }
+}
---
+++ LazyTests1/LazyTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Lazy.Tests
+{
+    [TestClass()]
+    public class LazyTests
+    {
+        private ILazy<int> lazy;
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            int number = 1;
+            lazy = LazyFactory<int>.CreateLazy(() => ++number);
+            Assert.AreEqual(2, lazy.Get());
+        }
+
+        [TestMethod()]
+        public void GetOnlyOnce()
+        {
+            int number = 1;
+            lazy = LazyFactory<int>.CreateLazy(() => ++number);
+            Assert.AreEqual(2, lazy.Get());
+            Assert.AreEqual(2, lazy.Get());
+        }
+    }
+}
