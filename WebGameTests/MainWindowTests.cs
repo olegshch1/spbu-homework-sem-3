126b6a19f61e80ff195c43cfa774a3b4215a7692
olegshch <st069439@student.spbu.ru>
2019-12-06 15:17:17 +0300

    как-то никак, но запускается

---
+++ WebGameTests/MainWindowTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using WebGame;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebGame.Tests
+{
+    [TestClass()]
+    public class MainWindowTests
+    {
+        private MainWindow game = new MainWindow();
+        
+
+        [TestMethod()]
+        public void MainWindowTest()
+        {
+            Assert.IsTrue(game);
+        }
+    }
+}
---
+++ WebGameTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью указанного ниже 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// общие сведения об этой сборке.
+[assembly: AssemblyTitle("WebGameTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("WebGameTests")]
+[assembly: AssemblyCopyright("Copyright ©  2019")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Установка параметра ComVisible в значение false делает типы в этой сборке невидимыми 
+// для компонентов модели COM.  Если тип в этой сборке необходимо сделать доступным 
+// модели COM, задайте для атрибута ComVisible этого типа значение true.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("08c50ac8-12fc-449e-b410-fee7615575c5")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [сборка: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
