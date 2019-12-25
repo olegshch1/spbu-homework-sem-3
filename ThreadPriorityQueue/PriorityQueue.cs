446462a4e34f56988a9d8373ff5b2b4487969222
olegshch <st069439@student.spbu.ru>
2019-12-25 13:11:21 +0300

    вроде работает

---
+++ ThreadPriorityQueue/PriorityQueue.cs
@@ -0,0 +1,104 @@
+﻿using System.Threading;
+
+namespace ThreadPriorityQueue
+{
+    /// <summary>
+    /// очередь с приоритетом
+    /// </summary>
+    public class PriorityQueue
+    {
+        /// <summary>
+        /// узел со значением и приоритетом
+        /// </summary>
+        private class Node
+        {
+            public int Data { get; set; }
+            public int Priority { get; set; }
+            public Node Next { get; set; }
+            public Node(int value, int priority)
+            {
+                Data = value;
+                Priority = priority;
+            }
+        }
+
+        private Node head;
+
+        public int Count { get; private set; }
+
+        public int Size() => Count;
+
+        private object locker = new object();
+
+        /// <summary>
+        /// получение узла
+        /// </summary>
+        /// <param name="priority">ближайший приоритет</param>
+        /// <returns>нужный узел</returns>
+        private Node Get(int priority)
+        {
+            lock (locker)
+            {
+                Node current = head;
+                if (Count < 2) return current;
+                for (int i = 0; i < Count - 1; i++)
+                {
+                    if (current.Next.Priority < priority)
+                    {
+                        return current;
+                    }
+                    current = current.Next;
+                }
+                return current.Next;
+            }
+        }
+
+        /// <summary>
+        /// добавление с блокированием
+        /// </summary>
+        /// <param name="value">значение</param>
+        /// <param name="priority">приоритет</param>
+        /// <returns>true при успешном добавлении</returns>
+        public bool Enqueue(int value, int priority)
+        {
+            lock (locker)
+            {
+                Node newNode = new Node(value, priority);
+                if (Count == 0)
+                {
+                    head = newNode;
+                }
+                else
+                {
+                    var current = Get(priority);
+                    newNode.Next = current.Next;
+                    current.Next = newNode;
+                }
+                Count++;
+                if (Count == 1)
+                {
+                    Monitor.PulseAll(locker);
+                }
+                return true;
+            }
+        }
+
+        /// <summary>
+        /// удаление с ожиданием в пустой очереди
+        /// </summary>
+        /// <returns></returns>
+        public int Deque()
+        {
+            lock (locker)
+            {
+                while (Count == 0)
+                {
+                    Monitor.Wait(locker);
+                }
+                int result = head.Data;
+                head = head.Next;
+                return result;
+            }
+        }
+    }
+}
---
+++ ThreadPriorityQueueTests/PriorityQueueTests.cs
@@ -0,0 +1,70 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Threading;
+
+namespace ThreadPriorityQueue.Tests
+{
+    [TestClass()]
+    public class PriorityQueueTests
+    {
+        private PriorityQueue queue = new PriorityQueue();
+
+        [TestMethod()]
+        public void SizeTest()
+        {
+            Assert.IsTrue(queue.Count==0);
+        }
+
+        [TestMethod()]
+        public void AddTest()
+        {
+            queue.Enqueue(5, 10);
+            queue.Enqueue(5, 5);
+            queue.Enqueue(6, 7);
+            Assert.AreEqual(3, queue.Count);
+        }
+
+        [TestMethod()]
+        public void DequeTest()
+        {
+            queue.Enqueue(5, 10);
+            queue.Enqueue(5, 5);
+            queue.Enqueue(6, 7);
+            Assert.AreEqual(5, queue.Deque());
+            Assert.AreEqual(6, queue.Deque());
+            Assert.AreEqual(5, queue.Deque());
+        }
+
+        [TestMethod()]
+        public void WorkingThread()
+        {
+            var thread1 = new Thread(() =>
+            {
+                queue.Deque();
+                queue.Enqueue(3, 11);
+                queue.Enqueue(2, 22);
+            });
+            var thread2 = new Thread(() =>
+            {
+                queue.Enqueue(13, 3);
+                queue.Deque();
+                queue.Enqueue(332, 222);
+            });
+            var thread3 = new Thread(() =>
+            {
+                queue.Deque();
+                queue.Enqueue(333, 11);
+                queue.Enqueue(2333, 2);
+            });
+
+            thread1.Start();
+            thread2.Start();
+            thread3.Start();
+
+            thread1.Join();
+            thread2.Join();
+            thread3.Join();
+
+            Assert.IsTrue(queue.Count > 0);
+        }
+    }
+}
