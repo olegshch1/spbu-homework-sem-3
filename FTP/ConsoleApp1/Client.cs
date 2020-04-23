32bea7da62b4651a578ad63e56f685eed8dcde2a
olegshch <st069439@student.spbu.ru>
2020-04-24 00:59:26 +0300

    withoit tests yet

---
+++ FTP/ConsoleApp1/Client.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Client class
+    /// </summary>
+    public class Client
+    {
+        private TcpClient client;
+        private StreamWriter writer;
+        private StreamReader reader;
+        private string host;
+        private int port;
+
+        public Client(string host, int port)
+        {
+            client = new TcpClient();
+            this.host = host;
+            this.port = port;
+        }
+
+        /// <summary>
+        /// Connecting
+        /// </summary>
+        public async Task Connect()
+        {
+            client.Connect(host, port);
+            var stream = client.GetStream();
+            writer = new StreamWriter(stream);
+            reader = new StreamReader(stream);
+        }
+
+        /// <summary>
+        /// Sending message
+        /// </summary>
+        /// <param name="message">message text</param>
+        private async Task Write(string message)
+        {
+            await writer.WriteLineAsync(message);
+        }
+
+        /// <summary>
+        /// Getting message
+        /// </summary>
+        private async Task<string> Read()
+        {
+            return await reader.ReadLineAsync();
+        }
+
+        /// <summary>
+        /// Closing client
+        /// </summary>
+        public void Close()
+        {
+            client.Close();
+        }
+
+        /// <summary>
+        /// Get command
+        /// </summary>
+        /// <param name="path">file path</param>
+        public async Task<(string, byte[])> Get(string path)
+        {
+            await Write("2 " + path);
+            int size = int.Parse(await Read());
+            if (size == -1)
+            {
+                return ("-1", null);
+            }
+            var data = new byte[size];
+            await reader.BaseStream.ReadAsync(data,0,size);
+            return (size.ToString(), data);
+        }
+
+        /// <summary>
+        /// Listing command
+        /// </summary>
+        /// <param name="path">current path</param>
+        public async Task<(string, List<(string, bool)>)> List(string path)
+        {
+            await Write("1 " + path);
+            var message = await Read();
+            if(message == "-1")
+            {
+                return ("-1", null);
+            }
+            var split = message.Split();
+            var list = new List<(string, bool)>();
+            for(int i=0; i < split.Length-1; i+=2)
+            {
+                list.Add((split[i], Convert.ToBoolean(split[i+1])));
+            }
+            return (message, list);
+        }
+    }
+}
---
+++ FTP/ConsoleApp1/Server.cs
@@ -0,0 +1,105 @@
+﻿using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    /// <summary>
+    /// Server class
+    /// </summary>
+    public class Server
+    {
+        private TcpListener listener;
+        private CancellationTokenSource token = new CancellationTokenSource();
+
+        public Server(int port)
+        {
+            listener = new TcpListener(IPAddress.Any, port);
+        }
+
+        /// <summary>
+        /// starting server
+        /// </summary>
+        public async Task Start()
+        {
+            listener.Start();
+            while (!token.IsCancellationRequested)
+            {
+                var client = await listener.AcceptTcpClientAsync();
+                await Task.Run(()=>Work(client));
+            }
+        }
+
+        /// <summary>
+        /// Stopping server
+        /// </summary>
+        public void Stop()
+        {
+            token.Cancel();
+        }
+
+        /// <summary>
+        /// Getting command and path
+        /// </summary>
+        /// <param name="message">pair of command and path in string format</param>
+        /// <returns></returns>
+        private (string, string) Parse(string message)
+        {
+            var pair = message.Split();
+            if (pair.Length < 2)
+            {
+                return (null, null);
+            }
+            return (pair[0], pair[1]);
+        }
+
+        /// <summary>
+        /// Making a list of file and directories in string format
+        /// </summary>
+        /// <param name="path">current path</param>
+        private string List(string path)
+        {
+            try
+            {
+                var dirInfo = new DirectoryInfo(path);
+                var files = dirInfo.GetFiles();
+                var dirNames = dirInfo.GetDirectories();
+                return files.Length + dirNames.Length + " " + string.Join(" ", files.Select(name => $"{name.Name} False")) + string.Join(" ", dirNames.Select(name => $"{name.Name} True"));
+            }
+            catch
+            {
+                return "-1";
+            }
+        }
+
+        /// <summary>
+        /// Main process
+        /// </summary>
+        /// <param name="client">current client</param>
+        private async Task Work(TcpClient client)
+        {
+            using(var stream = client.GetStream())
+            {
+                var writer = new StreamWriter(stream);
+                var reader = new StreamReader(stream);
+                var message = await reader.ReadLineAsync();
+                var (command, path) = Parse(message);
+                switch (command)
+                {
+                    case "1":
+                        await writer.WriteLineAsync(List(path));
+                        break;
+                    case "2":
+                        var data = File.OpenRead(path);
+                        await writer.WriteLineAsync(data.Length.ToString());
+                        data.CopyTo(writer.BaseStream);
+                        data.Close();
+                        break;
+                }
+            }
+        }
+    }
+}
