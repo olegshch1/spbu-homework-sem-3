2cfaf7ad8eba7c4b57bd14a541e57228c66ac451
olegshch <st069439@student.spbu.ru>
2020-04-24 03:31:41 +0300

    made interface

---
+++ ThreadPool/ThreadPool/IMyTask.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThreadPool
+{
+    /// <summary>
+    /// Interface for MyTask
+    /// </summary>
+    /// <typeparam name="TResult"></typeparam>
+    public interface IMyTask<TResult>
+    {
+        /// <summary>
+        /// true if task is finished
+        /// </summary>
+        bool IsCompleted { get; }
+
+        /// <summary>
+        /// rresult of task
+        /// </summary>
+        TResult Result { get; }
+
+        /// <summary>
+        /// new task which depends on result of current task
+        /// </summary>
+        IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult, TNewResult> func);
+    }
+}
