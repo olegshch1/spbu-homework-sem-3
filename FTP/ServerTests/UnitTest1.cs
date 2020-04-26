4849ca89e68e84f989a85ffb2c3c226ea486b596
olegshch <st069439@student.spbu.ru>
2020-04-26 07:28:53 +0300

    test file added

---
+++ FTP/ServerTests/UnitTest1.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using ConsoleApp1;
+namespace ServerTests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        private Server server;
+        private Client client;
+
+        [TestInitialize]
+        public void Initialize()
+        {
+            server = new Server(1234);
+            server.Start();
+            client = new Client("localhost", 1234);
+            client.Connect();
+        }
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
