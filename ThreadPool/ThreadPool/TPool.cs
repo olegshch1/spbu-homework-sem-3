4c1a0b3328756be6b27c3a991dab8fa83505d42e
olegshch <st069439@student.spbu.ru>
2020-04-28 18:21:55 +0300

    Update TPool.cs

--- ThreadPool/ThreadPool/TPool.cs
+++ ThreadPool/ThreadPool/TPool.cs
@@ -8,6 +8,9 @@ using System.Threading;
 
 namespace ThreadPool
 {
+    /// <summary>
+    /// MyThreadPool class
+    /// </summary>
     public class TPool
     {
         public int ThreadNumber { get; }
@@ -19,12 +22,19 @@ namespace ThreadPool
             StartThread();
         }
 
-        private Action Add(Action task)
+        /// <summary>
+        /// Adding task to ThreadPool queue
+        /// </summary>
+        public IMyTask<TResult> Add<TResult>(Func<TResult> func)
         {
-            taskqueue.Add(task);
+            var task = new MyTask<TResult>(func, this);
+            taskqueue.Add(task.Calculate());
             return task;
         }
 
+        /// <summary>
+        /// Starting with "ThreadNumber" number of threads
+        /// </summary>
         private void StartThread()
         {
             for(int i = 0; i < ThreadNumber; i++)
@@ -39,23 +49,43 @@ namespace ThreadPool
             }
         }
 
+        /// <summary>
+        /// MyTask class
+        /// </summary>
         private class MyTask<TResult> : IMyTask<TResult>
         {
+            private TPool pool;
             private object locker = new object();
             private Func<TResult> function;
+            private TResult result;
             public bool IsCompleted { get; set; }
 
-            public TResult Result { get; }
+            public TResult Result
+            {
+                get
+                {
+                    return result;
+                }
+            }
 
-            public MyTask(Func<TResult> task)
+            public MyTask(Func<TResult> task, TPool threadpool)
             {
                 function = task;
+                pool = threadpool;
             }
 
             public IMyTask<TNewResult> ContinueWith<TNewResult>(Func<TResult,TNewResult> func)
             {
-                var task = new MyTask<TNewResult>(()=>func(Result));
+                var task = new MyTask<TNewResult>(() => func(Result), pool);
                 return task;
+            }
+
+            public Action Calculate()
+            {
+                result = function();
+                IsCompleted = true;
+                function = null;
+                return result;
             }
         }
 
